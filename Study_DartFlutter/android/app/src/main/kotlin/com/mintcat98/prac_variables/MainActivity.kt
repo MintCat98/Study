bb6c5780b31aba261637fae6b65099462261a093
MintCat <presidentmc9898@gmail.com>
2023-02-01 14:55:22 +0900

    practice Android Studio

--- Study_DartFlutter/android/app/src/main/kotlin/com/mintcat98/prac_variables/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mintcat98.prac_variables
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ Study_Java/java-input/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+>
+java-input.iml,8\c\8c9a58dfcdba79c47762aba87c4cf54b2ec63e06
