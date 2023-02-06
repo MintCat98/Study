b6fcea07be96e3f059e16947a1b50969d8ff23cb
MintCat <presidentmc9898@gmail.com>
2023-02-06 17:00:08 +0900

    Practice: object of functions

---
+++ NomadCoder/JS_ChromeApp/230202-LearningJS/app.js
@@ -0,0 +1,16 @@
+const calculator = {
+    add: function (a, b) {
+        console.log(a + b);
+    },
+    minus: function (a, b) {
+        console.log(a - b);
+    },
+    times: function (a, b) {
+        console.log(a * b);
+    },
+    divide: function (a, b) {
+        console.log(a / b);
+    },
+};
+
+calculator.add(5, 5);
