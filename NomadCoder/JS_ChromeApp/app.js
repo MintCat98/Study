4bc83395d4601f02629cf811da48f9ac1726f493
MintCat <presidentmc9898@gmail.com>
2023-02-10 13:26:20 +0900

    Practice: classList.toggle

--- NomadCoder/JS_ChromeApp/app.js
+++ NomadCoder/JS_ChromeApp/app.js
@@ -1,14 +1,7 @@
 const title = document.querySelector(".title h1");
 
 function handleTitleClick() {
-  const clickClass = "click"; // raw value는 에러의 위험성이 있으니 되도록이면 변수를 설정하자!
-  const currentStyle = title.classList;
-
-  if (currentStyle.contains(clickClass)) {
-    currentStyle.remove(clickClass);
-  } else {
-    currentStyle.add(clickClass);
-  }
+  title.classList.toggle("clickColor");
 }
 
 title.addEventListener("click", handleTitleClick);
