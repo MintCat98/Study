50281f860280484e1d5c11de27b81d94bd602913
MintCat98 <presidentmc9898@gmail.com>
2023-02-27 17:12:20 +0900

    Upgrade: Save username to stop logging in

--- NomadCoder/JS_ChromeApp/app.js
+++ NomadCoder/JS_ChromeApp/app.js
@@ -3,15 +3,30 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function loginSubmit(event) {
   event.preventDefault();
 
   const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username); // key, value
   loginForm.classList.add(HIDDEN_CLASSNAME);
 
+  showGreeting(username);
+}
+
+function showGreeting(username) {
   greeting.innerText = `Hello, ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", loginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  // Show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", loginSubmit);
+} else {
+  // Show the greeting
+  showGreeting(savedUsername);
+}
