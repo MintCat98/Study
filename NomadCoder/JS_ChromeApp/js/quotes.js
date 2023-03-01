47068b0c0ae93aec501d4bb90916fcdacb989673
MintCat98 <presidentmc9898@gmail.com>
2023-03-01 13:59:46 +0900

    Add: Quotes

---
+++ NomadCoder/JS_ChromeApp/js/quotes.js
@@ -0,0 +1,54 @@
+const quotes = [
+  {
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
+  },
+  {
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+  },
+  {
+    quote:
+      "The world is a book and those who do not travel read only one page.",
+    author: "Saint Augustine",
+  },
+  {
+    quote: "Life is either a daring adventure or nothing at all.",
+    author: "Helen Keller",
+  },
+  {
+    quote: "To Travel is to Live",
+    author: "Hans Christian Andersen",
+  },
+  {
+    quote: "Only a life lived for others is a life worthwhile.",
+    author: "Albert Einstein",
+  },
+  {
+    quote: "You only live once, but if you do it right, once is enough.",
+    author: "Mae West",
+  },
+  {
+    quote: "Never go on trips with anyone you do ntot love.",
+    author: "Hemmingway",
+  },
+  {
+    quote: "We wander for distraction, but we travel for fulfilment.",
+    author: "Hilaire Belloc",
+  },
+  {
+    quote: "Travel expands the mind and fills the gap.",
+    author: "Sheda Savage",
+  },
+];
+
+const quote = document.querySelector("#quotes span:first-child"); // 첫 번째 span
+const author = document.querySelector("#quotes span:last-child"); // 두 번째 span
+
+const lengthQuoteArr = quotes.length;
+const quoteNum = Math.floor(Math.random() * lengthQuoteArr); // floor: 내림
+
+const todaysQuote = quotes[quoteNum];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
