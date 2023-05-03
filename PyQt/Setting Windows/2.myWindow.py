4684574048b885ebc28b129a9865d888db8988f0
MintCat98 <presidentmc9898@gmail.com>
2023-05-03 22:31:19 +0900

    Study: Add Button with PyQt5

--- PyQt/Setting Windows/2.myWindow.py
+++ PyQt/Setting Windows/2.myWindow.py
@@ -1,5 +1,4 @@
 import sys
-import typing
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtGui import QIcon
 
@@ -17,12 +16,10 @@ class MyWindow(QWidget):
         self.initUI()
 
     def initUI(self):
-        '''
-        QIcon object를 통해 사용할 아이콘 사진 가져오기
-        아이콘이 나타나지 않는 경우 -> 절대경로를 적으면 해결됨
-        '''
-        logo = QIcon(
-            '/Users/presi/git_laptop/Study/PyQt/Setting Windows/assets/ALFSR_logo.png')
+        # QIcon object를 통해 사용할 아이콘 사진 가져오기
+        # 아이콘이 나타나지 않는 경우 -> 절대경로를 적으면 해결됨
+        logo = QIcon('./assets/ALFSR_logo.png')
+        simple_btn = QPushButton('Print', self)
 
         # 윈도우 제목 변경
         self.setWindowTitle('My Window Test')
@@ -32,6 +29,17 @@ class MyWindow(QWidget):
         self.resize(750, 400)
         # 윈도우 아이콘 변경
         self.setWindowIcon(logo)
+
+        # simple_btn 설정
+        simple_btn.resize(200, 30)
+        # 얘는 윈도우랑 달리 위치 무조건 지정해줘야 함...
+        simple_btn.move(275, 185)
+        # JS의 eventListener처럼 눌렀을 때 실행이 되는 거라, 함수 뒤에 괄호 없이!
+        simple_btn.clicked.connect(self.simple_btn_func)
+
+    def simple_btn_func(self):
+        # 꼭 클래스 안에 버튼 관련 메서드가 있어야 하는 것은 아니지만, 클래스 내의 버튼에 관한 것이니 되도록이면 안에 넣기!
+        print('Peekaboo!')
 
 
 app = QApplication(sys.argv)
