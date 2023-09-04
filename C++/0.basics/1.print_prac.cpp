c697d808628e626059fe58c09a1d94e2b15140f0
MintCat <presidentmc9898@gmail.com>
2023-09-05 01:05:13 +0900

    Study: How to create new cpp files and use cout

---
+++ C++/0.basics/1.print_prac.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+// std 기능을 쓰려면 iostream이 필요함
+using namespace std;
+// std를 미리 지정해두면 cout을 쓸 때 굳이 안 써도 됨
+int main() {
+	cout << "티켓팅 망한 사람이 누구 ?" << endl;
+	cout << "누구긴, 나지";
+	return 0;
+}
---
+++ C++/0.basics/for_test.cpp
@@ -0,0 +1,5 @@
+//#include <iostream>
+//int main() {
+//	std::cout << "nou";
+//	return 0;
+//}
