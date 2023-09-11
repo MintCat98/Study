45adfa8ab2fa3f70539291435f1363c7b506c88a
MintCat <presidentmc9898@gmail.com>
2023-09-11 19:11:24 +0900

    fixed untracked files

--- C++/0.basics/1.print_prac.cpp
+++ C++/0.basics/1.print_prac.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
-// std              iostream    ʿ   
-using namespace std;
-// std    ̸       صθ  cout                  ᵵ   
-int main() {
-	cout << "Ƽ                     ?" << endl;
-	cout << "      ,     ";
-	return 0;
-}
+//#include <iostream>
+//// std              iostream    ʿ   
+//using namespace std;
+//// std    ̸       صθ  cout                  ᵵ   
+//int main() {
+//	cout << "Ƽ                     ?" << endl;
+//	cout << "      ,     ";
+//	return 0;
+//}
