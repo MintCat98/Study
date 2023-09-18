7ae235521ac0c8eba63d23d12a7a5e4c4e0051e6
MintCat <presidentmc9898@gmail.com>
2023-09-18 17:48:51 +0900

    C++ Bubble Sort: Fix an error / Implement `printArray`

--- C++/BubbleSort/main.cpp
+++ C++/BubbleSort/main.cpp
@@ -2,23 +2,25 @@
 using namespace std;
 
 // Function Declarations
-void mySort(int arr[], int n);
+//void mySort(int arr[], int n);
 void printArray(int arr[], int size);
 
 int main() {
 	// Test
 	int arr[] = { 3,1,2,5,6,7,10 };
 	int n = sizeof(arr) / sizeof(arr[0]);
-	mySort(arr, n);
+	//mySort(arr, n);
 	cout << "Sorted array: \n";
 	printArray(arr, n);
 	return 0;
 }
 
 // Function Definitions
-void mySort(int arr[], int n) {
+//void mySort(int arr[], int n) {
 
-}
+//}
 void printArray(int arr[], int size) {
-
+	for (short i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
 }
