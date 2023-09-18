2295c5b1c5c6ae396b72bab0f81eddc3afda2bca
MintCat <presidentmc9898@gmail.com>
2023-09-18 17:30:52 +0900

    C++ Bubble Sort: Format Complete

---
+++ C++/BubbleSort/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// Function Declarations
+void mySort(int arr[], int n);
+void printArray(int arr[], int size);
+
+int main() {
+	// Test
+	int arr[] = { 3,1,2,5,6,7,10 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	mySort(arr, n);
+	cout << "Sorted array: \n";
+	printArray(arr, n);
+	return 0;
+}
+
+// Function Definitions
+void mySort(int arr[], int n) {
+
+}
+void printArray(int arr[], int size) {
+
+}
