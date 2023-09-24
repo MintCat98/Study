66a5016db5157de6ceb4f210fa0ef2a61fd2975e
MintCat <presidentmc9898@gmail.com>
2023-09-25 00:22:29 +0900

    C++ Special Filter: Implementation success Failed to divide files - `main` / `specialfilter`

--- C++/SpecialStringFilter/SpecialFilter.cpp
+++ C++/SpecialStringFilter/SpecialFilter.cpp
@@ -1,3 +1,48 @@
-char** special_filter(const char* sentences[], int filter_offset, int num_sentences) {
+#include <iostream>
+#include <string.h>
+using namespace std;
+/*
+	ASCII: 65~90(cap) | 97~122
+	offset: 1~26
+	No special chars & Preserve whitespaces
 
+	Return: a pointer to a set of filtered sentences
+*/
+char** special_filter(const char* sentences[], int filter_offset, int num_sentences) {
+	// result container
+	char** filtered = new char* [num_sentences];
+	// divide sentences
+	for (int i = 0; i < num_sentences; i++) {
+		int len = strlen(sentences[i]);
+		filtered[i] = new char[len + 1];
+		// filter
+		for (int j = 0; i < len; j++) {
+			char select = sentences[i][j];
+			int ascii = int(select);
+			// whitespace or null
+			if (ascii == 32) {
+				filtered[i][j] = select;
+				continue;
+			}
+			else if (ascii == 0) {
+				filtered[i][j] = select;
+				break;
+			}
+			// letters
+			// cap or lower
+			int os = filter_offset;
+			int start = (ascii > 96) ? 97 : 65;
+			// lower
+			if (start > 96) {
+				os += 97;
+				filtered[i][j] = (ascii < os) ? select : char(35);
+			}
+			// cap
+			else {
+				os += 65;
+				filtered[i][j] = (ascii < os) ? select : char(35);
+			}
+		}
+	}
+	return filtered;
 }
--- C++/SpecialStringFilter/main.cpp
+++ C++/SpecialStringFilter/main.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
-#include "SpecialFilter.h"
+//#include "SpecialFilter.h"
+
+char** special_filter(const char* sentences[], int filter_offset, int num_sentences) {
+	// result container
+	char** filtered = new char* [num_sentences];
+	// divide sentences
+	for (int i = 0; i < num_sentences; i++) {
+		int len = strlen(sentences[i]);
+		filtered[i] = new char[len + 1];
+		// filter
+		for (int j = 0; i < len; j++) {
+			char select = sentences[i][j];
+			int ascii = int(select);
+			// whitespace or null
+			if (ascii == 32) {
+				filtered[i][j] = select;
+				continue;
+			}
+			else if (ascii == 0) {
+				filtered[i][j] = select;
+				break;
+			}
+			// letters
+			// cap or lower
+			int os = filter_offset;
+			int start = (ascii > 96) ? 97 : 65;
+			// lower
+			if (start > 96) {
+				os += 97;
+				filtered[i][j] = (ascii < os) ? select : char(35);
+			}
+			// cap
+			else {
+				os += 65;
+				filtered[i][j] = (ascii < os) ? select : char(35);
+			}
+		}
+	}
+	return filtered;
+}
 
 int main() {
 	const char* sentences[] = { "Hello bruhs", "I am ABCDEFGHIJKLMNOPQRSTUBWXYZ" };
